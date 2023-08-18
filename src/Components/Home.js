26a92d52b5f3345401695c26beea845e33024d90
Anbuindu <anbuindu1501@gmail.com>
2023-08-18 19:16:14 +0530

    First Commit

---
+++ src/Components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home(){
+    return(
+        <main>
+            
+            <div className="content">
+                <h1>Let's build Graphic designs that your users love</h1>
+                <p>Strong management and security for powerful features. Clean and Creative design for you.</p>
+                <button>Get Started</button>
+            </div>
+
+            <div className="img">
+                <img src="https://qph.cf2.quoracdn.net/main-qimg-7057f3bcced6a696e26a8cdad309c59b-lq" alt="" />
+            </div> 
+            
+        </main>
+    )
+}
+export default Home;
+
